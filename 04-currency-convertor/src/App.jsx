792d051911380f902eaa9d9a73014a130c5ee8d8
aditiiprasad <aditi03prasad@gmail.com>
2024-09-20 20:29:24 +0530

    new proj

---
+++ 04-currency-convertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-3xl bg-red-400'>Currency </h1>
+    </>
+  )
+}
+
+export default App
